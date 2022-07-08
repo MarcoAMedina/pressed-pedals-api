ec7fef59d7a6cead28cf5fe1a0199974ccf19c01
MarcoAMedina <medinaam94@gmail.com>
2022-07-08 03:43:34 -0500

    Populates seeds.rb with user, pedal, and favorite

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.create(name: "Marco", email: "test@test.com", password_digest: "password")
+pedal = Pedal.create(category: "Overdrive", name: "OCD", user_id: 1)
+favorite = Favorite.create(pedal_id: 1, user_id: 1)
