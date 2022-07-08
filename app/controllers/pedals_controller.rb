50cbf134f00bbb608b7dabc17d8c57aa274707cf
MarcoAMedina <medinaam94@gmail.com>
2022-07-08 12:17:57 -0500

    Adds more pedals to seeds.rb

--- app/controllers/pedals_controller.rb
+++ app/controllers/pedals_controller.rb
@@ -1,2 +1,6 @@
 class PedalsController < ApplicationController
+  def index
+    pedals = Pedal.all
+    render json: pedals.as_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 user = User.create(name: "Marco", email: "test@test.com", password_digest: "password")
 pedal = Pedal.create(category: "Overdrive", name: "OCD", user_id: 1)
+pedal = Pedal.create(category: "Tremolo", name: "Diamond", user_id: 1)
+pedal = Pedal.create(category: "Tuner", name: "Boss TU3 Chromatic Tuner", user_id: 1)
+pedal = Pedal.create(category: "Delay", name: "Digital Delay DD-8", user_id: 1)
+pedal = Pedal.create(category: "Reverb", name: "Line 6 DL4", user_id: 1)
 favorite = Favorite.create(pedal_id: 1, user_id: 1)
