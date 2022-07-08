2c3abd61fe3c73803ac9835f4f95c572c825e5c5
MarcoAMedina <medinaam94@gmail.com>
2022-07-08 04:12:29 -0500

    Implements b crypt gem and allows signup for new users with create action and route

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :loadouts
   has_many :favorites
   has_many :pedals, through: :favorites
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
 end
